ef35fd12e057309e77d8b584da07c615cfdb9c01
Mercenaryw <mercenaryw@gmail.com>
2021-05-17 00:56:00 -0300

    app de cadastro de series

---
+++ Series/Classes/EntidadeBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Series
+{
+    public abstract class EntidadeBase
+    {
+        //Atributos
+       public int Id { get; protected set; }
+
+    }
+}
